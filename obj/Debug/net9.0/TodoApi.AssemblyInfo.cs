f4b89ef719dcfb12d375f2692328b8bb187172d1
gadi.orellana <gadi.orellana@gmail.com>
2025-04-10 10:46:13 -0600

    agregando librerias de  EF

--- obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54787d609a388259105e8fe820c4ae777ba3b5f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b93ea30260e9b989ec13dde2507dffb823dfaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
